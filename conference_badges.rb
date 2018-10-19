cf9cc6b274e09fa629a7af36617cedecb3a311f2
Nicole D <nnardella@gmail.com>
2018-10-19 00:17:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+   names.map { |name| badge_maker(name) }
+end
+
+def assign_rooms
+
+
+
+end
